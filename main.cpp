e5b7ce59b58726f22ade968fd94cf5c751ce74cb
Swati <115008404+swatimry@users.noreply.github.com>
2025-02-10 21:20:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World! Printing diamond pattern" << std::endl;
+     int n = 5; 
+
+    
+    for (int i = 1; i <= n; i += 2) {
+        for (int j = 0; j < (n - i) / 2; j++)
+            std::cout << " ";
+        for (int j = 0; j < i; j++)
+            std::cout << "*";
+        std::cout << std::endl;
+    }
+
+   
+    for (int i = n - 2; i >= 1; i -= 2) {
+        for (int j = 0; j < (n - i) / 2; j++)
+            std::cout << " ";
+        for (int j = 0; j < i; j++)
+            std::cout << "*";
+        std::cout << std::endl;
+    }
+
     return 0;
 }
